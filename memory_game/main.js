25b12a0ab93458e5f37b3a7257bd9e6ea4fb564f
spiano <lafleurpiano@gmail.com>
2019-08-18 17:21:09 -0500

    Added more game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,21 +1,41 @@
-let cards = ['Queen', 'Queen', 'King', 'King'];
+let cards = [
+    {   rank: "Queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {   rank: "Queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {   rank: "King",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {   rank: "King",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 
 let cardsInPlay = [];
 
 function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
+        alert("You found a match!");
     } else {
-        console.log("Sorry, try again.");
+        alert("Sorry, try again.");
     }
 }
 function flipCard(cardId) {
-    cardsInPlay.push(cards[cardId]);
-    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
 }
 
 flipCard(0);
 flipCard(2);
 checkForMatch();
+
 
 
