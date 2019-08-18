97b8a3a8f08b21ee9ac3cc3b305dc009dcfeb7fb
spiano <lafleurpiano@gmail.com>
2019-08-18 14:04:12 -0500

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -2,17 +2,20 @@ let cards = ['Queen', 'Queen', 'King', 'King'];
 
 let cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("user flipped queen!");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("user flipped king!");
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+    } else {
+        console.log("Sorry, try again.");
+    }
+}
+function flipCard(cardId) {
+    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId]);
+}
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
 
 
-if (cardsInPlay[0] === cardsInPlay[2]){
-    alert("You found a match!");
-}
-else {
-    alert("Sorry, try again.");
-}
