6c7c8913ec0d82a0f5eb59fdbcdb0de6691fefc4
spiano <lafleurpiano@gmail.com>
2019-08-18 22:32:11 -0500

    everything up and working sort of.

--- memory_game/main.js
+++ memory_game/main.js
@@ -19,23 +19,35 @@ let cards = [
 
 let cardsInPlay = [];
 
+function flipCard() {
+    let cardId = this.getAttribute('data-id');
+    this.setAttribute('src', cards[cardId].cardImage);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+};
 function checkForMatch() {
+
     if (cardsInPlay[0] === cardsInPlay[1]) {
         alert("You found a match!");
     } else {
         alert("Sorry, try again.");
     }
-}
-function flipCard(cardId) {
-    cardsInPlay.push(cards[cardId].rank);
-    console.log("User flipped " + cards[cardId].rank);
-    console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);
+
 }
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+function createBoard(){
+    for (let i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', "images/back.png");
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+}
+
+createBoard();
+
 
 
 
